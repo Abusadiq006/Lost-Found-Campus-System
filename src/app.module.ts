d17c8021e4778b23d485bfa6647120c03450e510
Farmer Abusadiq <Abusadiq006@gmail.com>
2026-05-12 09:04:05 -0700

    initiating auth

---
+++ src/app.module.ts
@@ -0,0 +1,13 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+
+ConfigModule.forRoot({
+  isGlobal: true,
+});
+@Module({
+  imports: [],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
