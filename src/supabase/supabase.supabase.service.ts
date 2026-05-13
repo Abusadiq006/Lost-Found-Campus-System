b3afb8331e604f50fdf91d6d035731c7dd73bc25
Farmer Abusadiq <Abusadiq006@gmail.com>
2026-05-13 03:56:55 -0700

    @Inject

--- src/supabase/supabase.supabase.service.ts
+++ src/supabase/supabase.supabase.service.ts
@@ -3,6 +3,6 @@ import { Inject, Injectable, OnModuleInit } from '@nestjs/common'
 @Injectable()
 export class SupabaseService implements OnModuleInit {
     constructor(
-        
+        @Inject('SUPABASE_CLIENT')
     )
 }
