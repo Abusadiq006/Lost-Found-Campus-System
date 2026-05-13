691a490736e75be5096a2cd7a76b9b189f871406
Farmer Abusadiq <Abusadiq006@gmail.com>
2026-05-13 08:36:14 -0700

    SupabaseModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,11 +4,15 @@ import { ConfigModule } from '@nestjs/config'
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
+import { SupabaseModule } from './supabase/supabase.module';
+
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
     }),
+
+    SupabaseModule,
   ],
   controllers: [AppController],
   providers: [AppService],
