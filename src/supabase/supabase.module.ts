b2baad7c4d9c9a6b930521edee4c90c4957780c2
Farmer Abusadiq <Abusadiq006@gmail.com>
2026-05-13 08:27:56 -0700

    import SupabaseService

--- src/supabase/supabase.module.ts
+++ src/supabase/supabase.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common'
 import { SupabaseProvider } from './supabase.provider'
 
+import { SupabaseService } from './supabase.service'
 @Module({
     providers: [SupabaseProvider],
     exports: [SupabaseProvider],
