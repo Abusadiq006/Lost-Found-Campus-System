e5d2f11e89c18c181ba5b18cec18b6b55746f834
Farmer Abusadiq <Abusadiq006@gmail.com>
2026-05-14 09:47:00 -0700

    @Module

--- src/claims/claims.module.ts
+++ src/claims/claims.module.ts
@@ -4,3 +4,12 @@ import { SupabaseModule } from '../supabase/supabase.module';
 
 import { ClaimsController } from './claims.controller';
 import { ClaimsService } from './claims.service';
+
+@Module({
+  imports: [SupabaseModule],
+
+  controllers: [ClaimsController],
+
+  providers: [ClaimsService],
+})
+export class ClaimsModule {}
