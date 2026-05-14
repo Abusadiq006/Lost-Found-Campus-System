769f6ccebae98c1072aa52792cabeaf4d4b61fa6
Farmer Abusadiq <Abusadiq006@gmail.com>
2026-05-14 04:40:18 -0700

    new ValidationPipe

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,11 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.useGlobalPipes()
+  app.useGlobalPipes(
+    new ValidationPipe({
+      
+    })
+  )
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
