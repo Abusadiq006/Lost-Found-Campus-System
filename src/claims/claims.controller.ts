3750abd0268f47876b52c38fa32fa79cb3e83a01
Farmer Abusadiq <Abusadiq006@gmail.com>
2026-05-14 09:44:38 -0700

    Imports

--- src/claims/claims.controller.ts
+++ src/claims/claims.controller.ts
@@ -1,4 +1,17 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Param,
+  Patch,
+  Post,
+  UseGuards,
+} from '@nestjs/common';
 
-@Controller('claims')
-export class ClaimsController {}
+import { CurrentUser } from '../auth/current-user.decorator';
+import { SupabaseAuthGuard } from '../auth/supabase-auth.guard';
+
+import { ClaimsService } from './claims.service';
+
+import { CreateClaimDto } from './dto/create-claim.dto';
+import { ReviewClaimDto } from './dto/review-claim.dto';
