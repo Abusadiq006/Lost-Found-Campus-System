62526017104660fa93496445413ecdf5df3207ee
Farmer Abusadiq <Abusadiq006@gmail.com>
2026-05-14 05:36:50 -0700

    export class UsersService

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -8,3 +8,7 @@ import { SupabaseService } from '../supabase/supabase.service';
 import { UpdateUserDto } from './dto/update-user.dto'
 
 @Injectable()
+export class UsersService {
+  constructor(
+    private readonly supabaseService: SupabaseService,
+  ) {}
