79d0ab737a228c9b73b8e38c6d73902e0e374a11
Farmer Abusadiq <Abusadiq006@gmail.com>
2026-05-14 03:37:58 -0700

    request.user

--- src/auth/current-user.decorator.ts
+++ src/auth/current-user.decorator.ts
@@ -2,6 +2,7 @@ import { createParamDecorator, ExecutionContext } from '@nestjs/common';
 
 export const CurrentUser = createParamDecorator(
      (_: unknown, ctx: ExecutionContext) => {
-        const request = ctx.switchToHttp().getRequest();
+        const request = ctx.switchToHttp().getRequest()
+         return request.user;
      }
 )
