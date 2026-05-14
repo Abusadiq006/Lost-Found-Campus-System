5897584b93c14f6488f9613f0c3d02f9227c44fc
Farmer Abusadiq <Abusadiq006@gmail.com>
2026-05-14 09:37:14 -0700

    import

--- src/claims/claims.service.ts
+++ src/claims/claims.service.ts
@@ -1,4 +1,5 @@
-import { Injectable } from '@nestjs/common';
-
-@Injectable()
-export class ClaimsService {}
+import {
+  ForbiddenException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
