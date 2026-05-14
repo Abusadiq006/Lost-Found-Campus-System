24b5d69e6aab24821039eeb7cec595123dc40c6d
Farmer Abusadiq <Abusadiq006@gmail.com>
2026-05-14 09:37:34 -0700

    imports

--- src/claims/claims.service.ts
+++ src/claims/claims.service.ts
@@ -3,3 +3,11 @@ import {
   Injectable,
   NotFoundException,
 } from '@nestjs/common';
+
+import { SupabaseService } from '../supabase/supabase.service';
+
+import { CreateClaimDto } from './dto/create-claim.dto';
+import {
+  ClaimDecision,
+  ReviewClaimDto,
+} from './dto/review-claim.dto';
