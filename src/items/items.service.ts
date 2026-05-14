e5e8c33dd2437089f622ea102f5cfa5a6587fbfc
Farmer Abusadiq <Abusadiq006@gmail.com>
2026-05-14 06:31:31 -0700

    @injectable

--- src/items/items.service.ts
+++ src/items/items.service.ts
@@ -8,3 +8,14 @@ import { SupabaseService } from '../supabase/supabase.service';
 
 import { CreateItemDto } from './dto/create-item.dto';
 import { UpdateItemDto } from './dto/update-item.dto';
+
+@Injectable()
+export class ItemsService {
+  constructor(
+    private readonly supabaseService: SupabaseService,
+  ) {}
+
+  async create(
+    userId: string,
+    dto: CreateItemDto,
+  ) {
