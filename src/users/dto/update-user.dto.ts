0e03ccd024d99eac4e35114bb20b927ea987ac35
Farmer Abusadiq <Abusadiq006@gmail.com>
2026-05-14 04:47:54 -0700

    matric_number?: string

--- src/users/dto/update-user.dto.ts
+++ src/users/dto/update-user.dto.ts
@@ -5,9 +5,14 @@ import {
 } from 'class-validator'
 
 export class UpdateUserDto {
-  @IsOptional()
-  @IsString()
-  @MaxLength(100)
-  full_name?: string
+    @IsOptional()
+    @IsString()
+    @MaxLength(100)
+    full_name?: string
+
+    @IsOptional()
+    @IsString()
+    @MaxLength(50)
+    matric_number?: string
 
 }
