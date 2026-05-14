315798b59c8da755c922d40b11a58d834a56c117
Farmer Abusadiq <Abusadiq006@gmail.com>
2026-05-14 09:46:34 -0700

    imports in claims.module

--- src/claims/claims.module.ts
+++ src/claims/claims.module.ts
@@ -1,9 +1,6 @@
 import { Module } from '@nestjs/common';
+
+import { SupabaseModule } from '../supabase/supabase.module';
+
 import { ClaimsController } from './claims.controller';
 import { ClaimsService } from './claims.service';
-
-@Module({
-  controllers: [ClaimsController],
-  providers: [ClaimsService]
-})
-export class ClaimsModule {}
