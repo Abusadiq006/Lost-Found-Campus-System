cea758efefc0333f7db1881325245dca63798a98
Farmer Abusadiq <Abusadiq006@gmail.com>
2026-05-14 06:42:21 -0700

    @ in items controller

--- src/items/items.controller.ts
+++ src/items/items.controller.ts
@@ -16,3 +16,64 @@ import { CreateItemDto } from './dto/create-item.dto';
 import { UpdateItemDto } from './dto/update-item.dto';
 
 import { ItemsService } from './items.service';
+
+@Controller('items')
+export class ItemsController {
+  constructor(
+    private readonly itemsService: ItemsService,
+  ) {}
+
+  @Post()
+  @UseGuards(SupabaseAuthGuard)
+  async create(
+    @CurrentUser() user: any,
+
+    @Body() dto: CreateItemDto,
+  ) {
+    return this.itemsService.create(
+      user.id,
+      dto,
+    );
+  }
+
+  @Get()
+  async findAll() {
+    return this.itemsService.findAll();
+  }
+
+  @Get(':id')
+  async findOne(
+    @Param('id') id: string,
+  ) {
+    return this.itemsService.findOne(id);
+  }
+
+  @Patch(':id')
+  @UseGuards(SupabaseAuthGuard)
+  async update(
+    @Param('id') id: string,
+
+    @CurrentUser() user: any,
+
+    @Body() dto: UpdateItemDto,
+  ) {
+    return this.itemsService.update(
+      id,
+      user.id,
+      dto,
+    );
+  }
+
+  @Delete(':id')
+  @UseGuards(SupabaseAuthGuard)
+  async remove(
+    @Param('id') id: string,
+
+    @CurrentUser() user: any,
+  ) {
+    return this.itemsService.remove(
+      id,
+      user.id,
+    );
+  }
+}
