0dd7ea01e9204019ec44654530e03b63b1cd4c8e
Farmer Abusadiq <Abusadiq006@gmail.com>
2026-05-14 07:31:11 -0700

    imports in uploads.module

--- src/uploads/uploads.module.ts
+++ src/uploads/uploads.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
+
+import { SupabaseModule } from '../supabase/supabase.module';
+
 import { UploadsController } from './uploads.controller';
 import { UploadsService } from './uploads.service';
-
 @Module({
   controllers: [UploadsController],
   providers: [UploadsService]
