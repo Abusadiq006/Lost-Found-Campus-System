7fc8593c1bde72b8f6c08414c4d10e5206ad907a
Farmer Abusadiq <Abusadiq006@gmail.com>
2026-05-15 12:01:11 -0700

    async suspendUser

--- src/admin/admin.service.ts
+++ src/admin/admin.service.ts
@@ -16,3 +16,26 @@ import { SupabaseService } from '../supabase/supabase.service';
       .order('created_at', {
         ascending: false,
       });
+
+
+    if (error) {
+      throw new ForbiddenException(
+        error.message,
+      );
+    }
+
+    return data;
+  }
+
+  async suspendUser(userId: string) {
+    const supabase =
+      this.supabaseService.getClient();
+
+    const { data, error } = await supabase
+      .from('users')
+      .update({
+        is_suspended: true,
+      })
+      .eq('id', userId)
+      .select()
+      .single();
