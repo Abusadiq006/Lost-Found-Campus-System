cacd5dcea49b5c9b562a43881ec2e5840113f7a3
Farmer Abusadiq <Abusadiq006@gmail.com>
2026-05-15 11:59:35 -0700

    import in getAllUsers

--- src/admin/admin.service.ts
+++ src/admin/admin.service.ts
@@ -5,3 +5,14 @@ import {
 } from '@nestjs/common';
 
 import { SupabaseService } from '../supabase/supabase.service';
+
+ async getAllUsers() {
+    const supabase =
+      this.supabaseService.getClient();
+
+    const { data, error } = await supabase
+      .from('users')
+      .select('*')
+      .order('created_at', {
+        ascending: false,
+      });
